cea8f9d526a2e2a922ef1d618828f3188a8568ed
HJH3077 <wnsgur0657@naver.com>
2021-06-18 16:35:53 +0900

    20210618

---
+++ src/main/java/com/ict/db/DAO.java
@@ -0,0 +1,28 @@
+package com.ict.db;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class DAO {
+	private static SqlSession ss;
+	
+	private synchronized static SqlSession getSession() {
+		if (ss == null) {
+			ss = DBService.getFactory().openSession(false);
+		}
+		return ss;
+	}
+	
+	// 리스트
+	public static List<VO> getList(String category){
+		List<VO> list = null;
+		list = getSession().selectList("category", category);
+		return list;
+	}
+	
+}
+
